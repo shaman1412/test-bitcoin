8ac2d83b982588d2c228dbfc1afcd0151bd0cf61
Peeranat Jatupornpitak <>
2022-01-22 19:53:11 +0700

    Add init project

---
+++ src/main/kotlin/com/example/testbitcoin/TestBitcoinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testbitcoin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestBitcoinApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestBitcoinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/testbitcoin/TestBitcoinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.testbitcoin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestBitcoinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
